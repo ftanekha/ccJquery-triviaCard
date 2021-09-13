8d1ccfe9d20d8051e915a15c5e964b37dbb14a62
Farai Tanekha <30661410+codemusltd@users.noreply.github.com>
2021-09-13 14:58:12 +0100

    final commit, without suggested future challenges

--- main.js
+++ main.js
@@ -1,5 +1,35 @@
 $(document).ready(
     ()=>{
-        
+        //start with the hint box
+        $('.hint-box').on('click', () => {
+            //show hint
+            $('.hint').slideToggle('slow', 'swing', () => console.info('showing hint!'))
+        })
+
+        //identify wrong answers
+        const $wrongAnswers = ['.wrong-answer-one', '.wrong-answer-two', '.wrong-answer-three']
+        //animate wrong answers on click
+        for(let $wrongAnswer of $wrongAnswers){
+            $($wrongAnswer).on('click', () => {
+                //disappear smiley face (just in case)
+                $('.smiley').hide()
+                //
+                $(`${$wrongAnswer} .answer-text`).fadeOut('fast', 'linear', ()=> console.info('incrorrect answer detected!'))
+                //show frown image
+                $('.frown').show()
+            })
+        }
+
+        //animate correct answer
+        $('.correct-answer').on('click', () => {
+            //disappear incorrect answers
+            for(let $wrongAnswer of $wrongAnswers){
+                $($wrongAnswer).slideUp()
+            }
+            //disappear frown image
+            $('.frown').hide()
+            //show smiley face
+            $('.smiley').fadeIn('fast', 'linear', () => console.info('correct answers selected!!'))
+        })
     }
 )
